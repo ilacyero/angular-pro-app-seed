31409b29309972d2be4f39e4fa4d96e55531876f
Oscar Callisaya <oscar.callisaya@jalasoft.com>
2018-02-24 16:12:12 -0400

    6. Saved logged user in store.

--- src/auth/shared/services/auth/auth.service.ts
+++ src/auth/shared/services/auth/auth.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
-
 import { AngularFireAuth } from 'angularfire2/auth';
+
+import 'rxjs/add/operator/do';
+
+import { Store } from 'store';
 
 export interface User {
   email: string,
@@ -10,8 +13,25 @@ export interface User {
 
 @Injectable()
 export class AuthService {
+
+  auth$ = this.af.authState.do(
+    next => {
+      if (!next) {
+        this.store.set('user', null);
+        return;
+      }
+      const user: User = {
+        email: next.email,
+        uid: next.uid,
+        authenticated: true,
+      }
+      this.store.set('user', user);
+    }
+  )
+
   constructor(
     private af: AngularFireAuth,
+    private store: Store,
   ) { }
 
   createUser(email: string, password: string) {
