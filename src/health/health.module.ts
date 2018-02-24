286471390fc6b91aac455aa1fdb18ef97c3d48d5
Oscar Callisaya <oscar.callisaya@jalasoft.com>
2018-02-24 17:47:14 -0400

    7. Added schedule and workouts module.

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 export const ROUTES: Routes = [
+  { path: 'schedule', loadChildren: './schedule/schedule.module#ScheduleModule' },
   { path: 'meals', loadChildren: './meals/meals.module#MealsModule' },
-  // { path: 'schedule', loadChildren: './schedule/schedule.module#ScheduleModule' },
-  // { path: 'workouts', loadChildren: './workouts/workouts.module#WorkoutsModule' }
+  { path: 'workouts', loadChildren: './workouts/workouts.module#WorkoutsModule' },
 ];
 
 @NgModule({
---
+++ src/health/schedule/schedule.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ScheduleComponent } from './containers';
+
+export const ROUTES: Routes = [
+  { path: '', component: ScheduleComponent },
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(ROUTES),
+  ],
+  declarations: [
+    ScheduleComponent
+  ],
+})
+export class ScheduleModule {}
